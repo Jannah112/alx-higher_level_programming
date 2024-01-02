916de800cfeb3a0496fb268d910241059baaf21f
Jannah112 <alaaosman1635@gmail.com>
2024-01-02 08:17:15 -0800

    c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * check_cycle - bdjdjdjrhrhrjrj
+ * @list: ejjeejei
+ * Return: dieiei
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	if (!list)
+		return (0);
+	while (slow && fast && fast->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (1);
+		}
+	}
+	return (0);
+}
