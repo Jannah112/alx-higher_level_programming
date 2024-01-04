9fa94599adc28af49bd98327ba0d87435d66d9a9
Jannah112 <alaaosman1635@gmail.com>
2024-01-04 07:39:19 -0800

    c

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    len = len(sys.argv)
+    if len == 1:
+        print("0 arguments.")
+    elif len == 2:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(len - 1))
+        for i in range(1, len):
+            print("{}: {}".format(i, sys.argv[i]))
+
+
