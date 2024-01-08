b183783b22020bf88a8e92aec510a92854e96a52
Jannah112 <alaaosman1635@gmail.com>
2024-01-08 07:00:37 -0800

    c

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     x = ()
-    if len(sentences) == 0:
+    if len(sentence) == 0:
         x = 0, "None"
     else:
         x = len(sentence), sentence[0]
