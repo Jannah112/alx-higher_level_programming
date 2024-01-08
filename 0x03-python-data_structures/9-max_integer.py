9b712bb039b8589500a4a606da4a044890826e92
Jannah112 <alaaosman1635@gmail.com>
2024-01-08 06:14:28 -0800

    c

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        m = mylist[0]
+        for i in range(len(my_list)):
+            if my_list[i] > m:
+                m = my_list[i]
+        return (m)
